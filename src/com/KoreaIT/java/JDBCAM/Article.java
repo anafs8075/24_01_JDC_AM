61d581f230de38dd16ad2bcb4eacfde3bb59f477
anafs8075 <anafs8075@gmail.com>
2024-01-05 17:56:32 +0900

    JDBC select 테스트

--- src/com/KoreaIT/java/JDBCAM/Article.java
+++ src/com/KoreaIT/java/JDBCAM/Article.java
@@ -5,16 +5,42 @@ public class Article {
 	private int id;
 	private String title;
 	private String body;
+	private String regDate;
+	private String updateDate;
 
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
 	}
+	public Article(int id, String regDate, String updateDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+	}
 
 	@Override
 	public String toString() {
-		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
 	}
 
 	public int getId() {
